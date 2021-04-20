bffc11eab2ad0532abb9a824da06213473d89cc8
John Lyu <paleneutron@outlook.com>
2021-04-20 19:06:18 +0800

    fix quotes in string problem

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
   name = 'jupyter2clipboard',
   packages = ['jupyter2clipboard'],
-  version = '0.2.0',
+  version = '0.2.1',
   license = 'MIT',
   description = 'A simple utility to copy jupyter cell output to your local clipboard from a hosted instance of jupyter.',
   author = 'paleneutron',
